bb5dad0a68ed3f44262d8c2236e8f883e75d4244
amine <minouking95@gmail.com>
2019-11-21 22:56:41 +0100

    Update amine.js

--- amine.js
+++ amine.js
@@ -1,8 +1,4 @@
-/* -----------------------------------
-الإضافة من طرف مدونة خنشلة تك
-BlogName  :	 Khenchela Tech
-Url       :	 https://khe-tech.blogspot.com/
--------------------------------------- */
+
 !function(){function f(){var a=document.createElement("div");a.id="levelmaxblock";a.innerHTML='
 <div class="inner">
     <div class="header">
